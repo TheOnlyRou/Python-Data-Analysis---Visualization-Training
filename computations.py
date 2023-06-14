78b029dbbef5dbb8d525d9c8ae4d15459e64bc2a
Marouane Mokhles <marouanemokhles@yahoo.com>
2023-06-14 11:56:57 +0300

    Updated project after computations section

---
+++ computations.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+
+def compute_basic(df: pd.DataFrame):
+    print(df.min())
+    print(df.max())
+    print(df.sum())
+    # sum only numerical values
+    print(df.sum(numeric_only=True))
+    print(df.count())
+
+    # mean, mode and median
+    print(df.mean())
+    print(df.mode())
+    print(df.median())
+
+    # mean, mode and median with numeric values only. Notice the difference
+    print(df.mean(numeric_only=True))
+    print(df.mode(numeric_only=True))
+    print(df.median(numeric_only=True))
+
+    # Describing Numeric fields within the dataframe. Displays descriptive stats like percentiles, std, count,
+    # variance, etc
+    print(df.describe())
+
+    # Include object datatype or more in the describe command:
+    # Object can also be written as "O"
+    print(df.describe(include="object"))
+    # Include Objects and int64
+    print(df.describe(include=["object", "int64"]))
+
+    # get sum of population of us in 2010 census
+    # Dataset includes populations in whole of US, west, east regions and Puerto Rico. Here we remove those.
+    df.tail(52).head(51).sum(numeric_only=True)
+
+
+
+def exercise(df: pd.DataFrame):
+    print(f"Lowest User Rating: \n{df['User Rating'].min()}")
+    print(f"Highest Price: \n{df['Price'].max()}")
+    print(f"Average User Rating: \n{df['User Rating'].mean()}")
+    print(f"Average User Rating of first 5: \n{df['User Rating'].head().mean()}")
+    print(f"Most occurring User Review: \n{df['Reviews'].mode()}")
+    print(f"Unique Number of Authors: \n{df['Author'].describe().loc[['unique']]}")
+    print(f"Author with most books: \n{df['Author'].describe().loc[['top']]}")
+
---
+++ dataframes.py
@@ -0,0 +1,144 @@
+import pandas as pd
+
+
+def get_houses_example():
+    # id` - house's unique id"
+    # date` - sale date",
+    # price` - sale price",
+    # bedrooms` - number of bedrooms",
+    # bathrooms` - numbers of bathrooms",
+    # sqft_living` - living space square footage ",
+    # sqft_lot` - total lot square footage",
+    # floors` - numbers of floors",
+    # waterfront` - is the house waterfront (1) or not (0)",
+    # view` - rating from 0 to 4 of how good the view from the house is",
+    # condition` - rating from 1 (poor) to 5 (very good) of the condition of the house",
+    # grade` - rating from 1-13 representing the construction quality of improvements. 1-3 Falls short of minimum
+    #               building standards (cabins, etc.) 7 is avg grade, 11-13 have high-quality design & construction",
+    # sqft_above` - square footage of the interior that is above ground level",
+    # sqft_basement` - square footage of the interior that is below ground level",
+    # yr_built` - year the house was initially built",
+    # yr_renovated` - The year of the house’s last renovation (if any)",
+    # zipcode` - zipcode that the house is located in",
+    # lat` - the property's latitude",
+    # long` - the property's longitude",
+    # sqft_living15` - average interior space square footage of the nearest 15 neighbors",
+    # sqft_lot15` - average lot square footage of the nearest 15 neighbors"
+    houses_df = pd.read_csv("D:\\OneDevelopment\\Online Courses\\Data Visualization Analysis in "
+                            "Python\\data\\kc_house_data.csv")
+    return houses_df
+
+
+def get_states_example():
+    states_df = pd.read_csv("D:\\OneDevelopment\\Online Courses\\Data Visualization Analysis in "
+                            "Python\\data\\states.csv")
+    return states_df
+
+
+def get_netflix_example():
+    # Reading a csv file with a different separator and defiing an existing index column in the csv file
+    netflix_df = pd.read_csv("D:\\OneDevelopment\\Online Courses\\Data Visualization Analysis in "
+                             "Python\\data\\netflix_titles.csv", sep="|", index_col=0)
+    return netflix_df
+
+def nst_est_example():
+    # Reading a csv file with header names that we want to replace, so we set header=0, then provide the new names in
+    # names as a list
+    nst_est_df = pd.read_csv("D:\\OneDevelopment\\Online Courses\\Data Visualization Analysis in "
+                             "Python\\data\\nst-est2020.csv", names=['sumlev', 'region', 'division', 'state', 'name',
+                                                                     'census2010pop', 'estimatesbase2010',
+                                                                     'popestimate2010', 'popestimate2011',
+                                                                     'popestimate2012', 'popestimate2013',
+                                                                     'popestimate2014', 'popestimate2015',
+                                                                     'popestimate2016', 'popestimate2017',
+                                                                     'popestimate2018', 'popestimate2019',
+                                                                     'popestimate042020', 'popestimate2020'], header=0)
+
+
+def get_titanic_example():
+    # Import the titanic dataset
+    titanic_df = pd.read_csv("D:\\OneDevelopment\\Online Courses\\Data Visualization Analysis in "
+                             "Python\\data\\titanic.csv")
+    return titanic_df
+
+
+def get_bestsellers_example():
+    bestsellers_df = pd.read_csv("D:\\OneDevelopment\\Online Courses\\Data Visualization Analysis in "
+                                 "Python\\data\\bestsellers.csv")
+    return bestsellers_df
+
+
+def get_everest_example():
+    everest_df = pd.read_csv("D:\\OneDevelopment\\Online Courses\\Data Visualization Analysis in "
+                             "Python\\data\\mount_everest_deaths.csv", index_col=0)
+    return everest_df
+
+
+def get_movietitles_example():
+    # Reading a delimiter separated value file with a difference separator: \t
+    names = ["id", "title", "year", "imdb_rating", "imdb_id", "genres"]
+    movie_titles_df = pd.read_csv("D:\\OneDevelopment\\Online Courses\\Data Visualization Analysis in "
+                                  "Python\\data\\movie_titles.tsv", sep="\t", names=names)
+    return movie_titles_df
+
+
+def exercise():
+
+    print(get_houses_example().shape)
+    print(get_houses_example().size)
+
+    # creates a new dataframe with n number of rows from the head (start) of the dataframe
+    print(get_houses_example().head(2))
+    # creates a new dataframe with n number of rows from the tail (end) of the dataframe
+    print(get_houses_example().tail(2))
+
+    # Displays dataframe column information (Non-null, Datatype)
+    print(get_houses_example().info())
+
+    # Displays only datatype information about columns in the dataframe
+    print(get_houses_example().dtypes)
+
+
+    print(get_movietitles_example().tail(7))
+
+    print(get_everest_example().shape)
+    print(get_everest_example().info())
+    # Columns 0 & 1 have 0 null values
+    # Expedition column has most null values (39)
+
+    print(get_bestsellers_example().shape)
+    print(get_bestsellers_example().size)
+    print(get_bestsellers_example().info())
+    print(get_bestsellers_example().dtypes)
+    print(get_bestsellers_example().head())
+    print(get_bestsellers_example().head(19))
+    print(get_bestsellers_example().tail())
+    print(get_bestsellers_example().tail(2))
+    # 550 rows, 7 columns
+    # No missing values
+    # Float64
+    # 3 Integer Columns
+
+
+    # pclass` - Passenger Class (1 = 1st; 2 = 2nd; 3 = 3rd)",
+    # survived` - Survival (0 = No; 1 = Yes)",
+    # name` - Name",
+    # sex` - Sex",
+    # age` - Age",
+    # sibsp` - Number of Siblings/Spouses Aboard",
+    # parch` - Number of Parents/Children Aboard",
+    # ticket` - Ticket Number",
+    # fare` - Passenger Fare",
+    # cabin` - Cabin",
+    # embarked` - Port of Embarkation (C = Cherbourg; Q = Queenstown; S = Southampton)",
+    # boat` - Lifeboat (if survived)",
+    # body` - Body number (if did not survive and body was recovered)",
+    # home.dest` - Home/Destination"
+    print(get_titanic_example().head(2))
+    print(get_titanic_example().tail(2))
+    print(get_titanic_example().dtypes)
+
+    print(get_netflix_example().head(2))
+    print(get_netflix_example().tail(2))
+    print(get_netflix_example().dtypes)
+
